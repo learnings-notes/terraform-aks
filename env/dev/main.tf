f44fe33499a3d18d1215791827c2e176a7796165
jamalikake <jamalirkp@gmail.com>
2022-07-23 00:09:06 +0530

    created VNet in main module

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -9,3 +9,12 @@ terraform {
     key                  = "aks-Dev.tfstate"
   }
 }
+
+module "network" {
+  source              = "Azure/network/azurerm"
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = "10.52.0.0/16"
+  subnet_prefixes     = ["10.52.0.0/24"]
+  subnet_names        = ["subnet1"]
+  depends_on          = [azurerm_resource_group.rg]
+}
